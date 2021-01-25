7bc9b8b3fab42b838e0f71b3fa64e1da85d9d002
Chiahui C <67808568+chiahuichen@users.noreply.github.com>
2021-01-25 03:15:01 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+credentials = {'username':"rmd28864",
+               'password': "s12kwr5^4bx8ncwk",
+               'ibm_db_sa':'rmd28864:s12kwr5%5E4bx8ncwk@dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50000/BLUDB'}
